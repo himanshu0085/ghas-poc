d176f91dab3e6b2d2db67747337c67da46107866
Himanshu Parashar <207495493+himanshu0085@users.noreply.github.com>
2025-11-26 14:49:13 +0530

    Create vuln.py

---
+++ codeql-poc/vuln.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/run")
+def run():
+    cmd = request.args.get("cmd")
+    return os.popen(cmd).read()   # 🚨 command injection
+
+if __name__ == "__main__":
+    app.run()
